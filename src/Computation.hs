1e1ee552e8d72207196181e4e333ab296455d601
Giacomo Tesio <giacomo@tesio.it>
2010-10-17 18:07:52 +0200

    removed the Expression type with a foldr approach

--- src/Computation.hs
+++ src/Computation.hs
@@ -21,24 +21,8 @@ module Computation (
     evaluate
 ) where
 
-isResult :: Expression a -> Bool
-isResult (Result r) = True
-isResult _ = False
-
-
 evaluate :: (Show a) =>  (String -> Computation a) -> [String] -> Either String a
-evaluate language tokens = compute $ current result
-    where result = until isResult (evaluateExpression language) $ Expression Empty tokens
-
-data Expression a =
-      Result     {current :: Computation a}
-    | Expression {current :: (Computation a),
-                  rest :: [String]
-                 }
-
-instance (Show a) => Show (Expression a) where
-    show (Result r) = "Result: " ++ show r
-    show (Expression r rest) = "Expression (" ++ show r ++ ") " ++ show rest
+evaluate language tokens = compute $ foldr (\s acc -> apply acc $ language s) Empty tokens
 
 data Computation a =
       Empty
@@ -57,27 +41,18 @@ instance (Show a) => Show (Computation a) where
     show (Function f) = "Function"
     show (Error e) = "Error: " ++ e
 
-evaluateExpression :: (Show a) => (String -> Computation a) -> Expression a -> Expression a
-evaluateExpression _ (Expression result []) = Result result
-evaluateExpression parse (Expression (Function f) rest) = Expression (Function f) rest
-evaluateExpression parse (Expression result (x:xs))
-    | isResult nextExp = Result $ apply result $ current nextExp
-    | otherwise = evaluateExpression parse $ Expression (apply result $ current nextExp) $ rest nextExp
-    where nextExp = evaluateExpression parse $ Expression (parse x) xs
-
 apply :: (Show a) => Computation a -> Computation a -> Computation a
-apply Empty (Value x) = Value x
-apply Empty (Function f)  = parseError "Too few arguments."
-apply Empty (Error e)  = (Error e)
-apply (Value x) (Value y) = parseError "Too many arguments."
-apply (Value x) (Function f) = f x
-apply (Function f) (Function g)  = parseError "Too many functions."
+apply Empty c = c
 apply (Error e) _ = Error e
-apply x y = parseError $ "Can not apply " ++ show x ++ " to " ++ show y ++ "."
+apply (Value x) (Value y) = parseError "Too many arguments."
+apply (Function f) Empty  = parseError "Too few arguments."
+apply (Function f) (Value x) = f x
+apply (Function f) (Function g)  = Function $ \x -> apply (Function f) (g x)
+apply (Value x) (Function f) = parseError "Missing operator."
 
 compute :: Computation a -> Either String a
 compute (Value x) = (Right x)
-compute (Function f) = Left "Missing one argument."
+compute (Function f) = Left "Missing arguments."
 compute (Error e) = Left e
 compute Empty = Left "No computation required."
 
